72f8ba5e3dd19ff3d684ee354fa802cf3127d1c1
KashafLareb <kashaflareb@gmail.com>
2022-03-22 13:15:05 +0500

    Add Article module

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_22_080209) do
+ActiveRecord::Schema.define(version: 2022_03_22_081224) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_articles_on_category_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "category_name"
@@ -19,4 +27,5 @@ ActiveRecord::Schema.define(version: 2022_03_22_080209) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "articles", "categories"
 end
