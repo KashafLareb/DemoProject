c55faa2360b53115262332f4f715b9cb9c86adc9
KashafLareb <kashaflareb@gmail.com>
2022-03-22 23:35:55 +0500

    Try to display action_text in active_admin

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -1,16 +1,17 @@
 ActiveAdmin.register Article do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  permit_params :title, :category_id
-  #
-  # or
-  #
+  permit_params :title, :content, :category_id
+
+  # form do |f|
+  #   f.inputs 'Article' do
+  #     f.input :title
+  #     f.rich_text_area :content
+  #   end
+  #   f.actions
+  # end
+  
   # permit_params do
-  #   permitted = [:title, :category_id]
+  #   permitted = [:title, :content, :category_id]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
