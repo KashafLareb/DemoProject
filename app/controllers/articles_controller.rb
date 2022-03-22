41612c2012a281a62077d94bc3433fc5c1e31c43
KashafLareb <kashaflareb@gmail.com>
2022-03-22 22:16:14 +0500

    Permit :content in article_params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -70,6 +70,6 @@ class ArticlesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def article_params
-      params.require(:article).permit(:title, :category_id)
+      params.require(:article).permit(:title, :content, :category_id)
     end
 end
