2f378fd928156cefae84a81992fd05a2d8dc8feb
KashafLareb <kashaflareb@gmail.com>
2022-03-25 06:06:34 +0500

    Define comments controller actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+  def index
+    @article = Article.find(params[:article_id])
+    @comments = @article.comments
+  end
+  def create
+    @user = current_user
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(params[:comment].permit(:body, :article_id))
+    @comment.user_id = @user.id
+    @comment.save
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
 end
