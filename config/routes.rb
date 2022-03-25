8e36136234b2d33f55b3d5bf85762affbc2f50fc
KashafLareb <kashaflareb@gmail.com>
2022-03-25 06:05:27 +0500

    Set routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   root "categories#index"
   resources :categories
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
