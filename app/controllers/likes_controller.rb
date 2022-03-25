0458c84fffb48659080badc8c888a565449ed317
KashafLareb <kashaflareb@gmail.com>
2022-03-25 13:07:46 +0500

    Define likes controller actions

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,18 @@
 class LikesController < ApplicationController
+  def create
+    if already_liked?
+      @like = Like.where(user_id: current_user.id, article_id: @article.id)
+      @like.destroy_all
+    else
+      @article = Article.find(params[:article_id])
+      @like = Like.create
+      @like.article_id=@article.id
+      @like.user_id=current_user.id
+      @like.save
+    end
+  end
+  def already_liked?
+    @article = Article.find(params[:article_id])
+    Like.where(user_id: current_user.id, article_id: @article.id).exists?
+  end
 end
