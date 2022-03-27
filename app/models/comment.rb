991897db9ce347d0c1244507aecf307afcf1fabe
KashafLareb <kashaflareb@gmail.com>
2022-03-28 01:45:59 +0500

    Correct comment format (article search bar in AA)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   has_rich_text :content
   validates :content, presence: true, length: { minimum: 5 }
+  def to_s
+    self.content
+  end
 end
