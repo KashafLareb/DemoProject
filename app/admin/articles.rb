451b025d74e2e8976a8b44487176e08ac6a9e71a
KashafLareb <kashaflareb@gmail.com>
2022-03-28 01:14:46 +0500

    Enable rich text in active admin form

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -2,13 +2,15 @@ ActiveAdmin.register Article do
 
   permit_params :title, :content, :category_id
 
-  # form do |f|
-  #   f.inputs 'Article' do
-  #     f.input :title
-  #     f.rich_text_area :content
-  #   end
-  #   f.actions
-  # end
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs do
+      f.input :category
+      f.input :title
+      f.input :content, as: :action_text
+    end
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
   
   # permit_params do
   #   permitted = [:title, :content, :category_id]
