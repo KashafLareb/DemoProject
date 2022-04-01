8d1e8783e8397da6736791d985236e9fafd0dd76
KashafLareb <kashaflareb@gmail.com>
2022-04-01 14:16:14 +0500

    Set comment serializer

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,4 +1,12 @@
 class CommentSerializer
   include JSONAPI::Serializer
-  attributes :id
+  attribute :comment_body do |comment|
+    comment.content.body
+  end
+  attribute :commenter do |comment|
+    User.find(comment.user_id).email
+  end
+  attribute :created_at do |comment|
+    (User.find(comment.user_id).created_at).to_formatted_s(:short)
+  end
 end
