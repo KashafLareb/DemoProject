7f9ec5a7864a583873def4436f41a01fac4c5436
KashafLareb <kashaflareb@gmail.com>
2022-04-01 14:23:51 +0500

    Set article serializer

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,6 +1,8 @@
 class ArticleSerializer
   include JSONAPI::Serializer
   
-  attributes :id, :title
-  has_many :comments
+  attributes :title
+  attribute :article_content do |article|
+    article.content.body
+  end
 end
