5629313a7da22b575d30ccbadf8a2b555cfbfef1
KashafLareb <kashaflareb@gmail.com>
2022-04-04 23:33:03 +0500

    Add email confirmation and password reset features

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable
   
   def self.authenticate(email, password)
     user = User.find_by(email: email)
