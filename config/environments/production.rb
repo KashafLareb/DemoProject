c9a796f730a895f120db8542e3ad3237ca5d5990
KashafLareb <kashaflareb@gmail.com>
2022-04-09 05:16:24 +0500

    Add gmail settings for production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,16 @@
 Rails.application.configure do
+  # Gmail settings
+  config.action_mailer.default_url_options = { host: 'quiet-island-81181.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 25,
+    domain:               'gmail.com',
+    user_name:            'quiet.island.81181@gmail.com',
+    password:             'livyguttzspfqfdt',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -27,19 +39,6 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-
-  ## Gmail settings
-  # config.action_mailer.default_url_options = { host: 'herokuapp.com' }
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address:              'smtp.gmail.com',
-  #   port:                 25,
-  #   domain:               'gmail.com',
-  #   user_name:            'quiet.island.81181@gmail.com',
-  #   password:             'owhcfipvquynvphl',
-  #   authentication:       'plain',
-  #   enable_starttls_auto: true
-  # }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
